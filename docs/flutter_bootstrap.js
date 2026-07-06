7cf9899b72af54737349d2babb0480a951853201
HP <kjhamja77@gmail.com>
2026-07-06 10:41:10 +0400

    Update web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1337236424" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3225044409" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
